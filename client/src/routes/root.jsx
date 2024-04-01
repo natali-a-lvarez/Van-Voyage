824219c6a5bd32d36c9171f974de8cebe952f93f
natali-a-lvarez <03nataliaalvarez07@gmail.com>
2024-04-01 14:29:38 -0400

    adding filter by location functionality

--- client/src/routes/root.jsx
+++ client/src/routes/root.jsx
@@ -6,6 +6,7 @@ import "./root.css";
 export default function Root() {
   const [vans, setVans] = useState([]);
   const [filter, setFilter] = useState("");
+  const [filteredVans, setFilteredVans] = useState([]);
 
   async function fetchVans() {
     fetch("http://127.0.0.1:5000/vans")
@@ -17,31 +18,45 @@ export default function Root() {
 
   useEffect(() => {
     fetchVans();
-  }, []);
+  }, [vans]);
 
-  function filterVans(van) {}
+  function filterVans(e) {
+    setFilter(e.target.value);
+    const result = vans.filter((van) => van.location === e.target.value);
+    setFilteredVans(result);
+  }
 
   return (
     <>
       <Navigation />
+      {vans.length === 0 && <p className="error-message">No vans to rent.</p>}
       <div className="van-list">
-        {vans.length === 0 && <p className="error-message">No vans to rent.</p>}
-        <div className="filter-container">
-          <form>
-            <label>Filter by</label>
-            <select onChange={(e) => setFilter(e.target.value)}>
-              <option value="">All</option>
-              <option value="Seattle, Washington">Seattle, Washington</option>
-              <option value="San Francisco, California">
-                San Francisco, California
-              </option>
-              <option value="Denver, Colorado">Denver, Colorado</option>
-            </select>
-          </form>
-        </div>
-        {vans.map((van) => (
-          <Van key={van.id} van={van} fetchVans={fetchVans} />
-        ))}
+        <form className="filter-container">
+          <select onChange={(e) => filterVans(e)}>
+            <option value="" disabled selected>
+              Filter by
+            </option>
+            <option value="">All</option>
+            <option value="Seattle, Washington">Seattle, Washington</option>
+            <option value="San Francisco, California">
+              San Francisco, California
+            </option>
+            <option value="Denver, Colorado">Denver, Colorado</option>
+          </select>
+        </form>
+
+        {filter === "" &&
+          vans.map((van) => (
+            <Van key={van.id} van={van} fetchVans={fetchVans} />
+          ))}
+        {filter != "" &&
+          filteredVans.length > 0 &&
+          filteredVans.map((van) => (
+            <Van key={van.id} van={van} fetchVans={fetchVans} />
+          ))}
+        {filter != "" && filteredVans.length === 0 && (
+          <p className="error-message">No vans for this location.</p>
+        )}
       </div>
     </>
   );
